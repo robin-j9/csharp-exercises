3a47a7bd93b160e3020e4d87d3e79ed272f23056
robin-j9 <robin@robinjun.com>
2020-01-15 19:33:26 -0600

    completed bonuses for class 2 studio

--- CountingCharacters/Program.cs
+++ CountingCharacters/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
+using System.IO;
 
 namespace CountingCharacters
 {
@@ -10,9 +11,12 @@ namespace CountingCharacters
         {
             //string textBlock = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus.";
             
+            // Bonus 4
+            string textBlock = System.IO.File.ReadAllText(@"C:\Users\Juno\Desktop\LC projects\robin-j9\csharp-exercises\string.txt");
+
             // Bonus 3
-            Console.WriteLine("Enter string:");
-            string textBlock = Console.ReadLine();
+            //Console.WriteLine("Enter string:");
+            //string textBlock = Console.ReadLine();
 
             // Bonus 1
             textBlock = textBlock.ToLower();
