efe1242507509924ec1169d1a577e4f53e1cec40
robin-j9 <robin@robinjun.com>
2020-02-12 20:08:11 -0600

    completed class 5 exercises and Restaurant Menu studio

---
+++ Restaurant/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Restaurant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Menu
+    {
+        public List<MenuItem> MenuItems { get; set; } = new List<MenuItem>();
+        public DateTime LastUpdated { get; set; }
+
+        public void Add(MenuItem item)
+        {
+            MenuItems.Add(item);
+        }
+
+        public void Remove(MenuItem item)
+        {
+            MenuItems.Remove(item);
+        }
+    }
+
+    public enum Category { Appetizer, MainCourse, Desert }
+    public class MenuItem
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public Category ItemCategory { get; set; }
+        public bool IsNew { get; }
+
+        public MenuItem(double price, string description, Category category, bool isNew)
+        {
+            Price = price;
+            Description = description;
+            ItemCategory = category;
+            IsNew = isNew;
+        }   
+    }
+}
---
+++ School/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace School
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Student
+    {
+        private double gpa;
+
+        public string Name { get; set; }
+        public int StudentId { get; set; }
+        public int NumberOfCredits { get; set; }
+        public double Gpa 
+        {
+            get { return gpa; }
+            internal set { gpa = value; } 
+        }
+
+        public Student(string name, int studentId, int numberOfCredits, double gpa)
+        {
+            NumberOfCredits = numberOfCredits;
+            StudentId = studentId;
+            Gpa = gpa;
+        }
+
+        public Student(string name, int studentId)
+            : this (name, studentId, 0, 0) { }
+    }
+
+    public class Course
+    {
+        public string CourseName { get; set; }
+        public string CourseCode { get; set; }
+        public int CreditHours { get; set; }
+        public List<Student> StudentRoster { get; set; }
+        
+    }
+}
