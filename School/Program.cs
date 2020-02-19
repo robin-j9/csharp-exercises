dba97c081325b376ab7f8cd7fb8cb647d8041a4a
robin-j9 <robin@robinjun.com>
2020-02-19 03:40:31 -0600

    completed class 6 prep exercises

--- School/Program.cs
+++ School/Program.cs
@@ -13,16 +13,11 @@ namespace School
 
     public class Student
     {
-        private double gpa;
-
+        private static int nextStudentId = 1;
         public string Name { get; set; }
         public int StudentId { get; set; }
         public int NumberOfCredits { get; set; }
-        public double Gpa 
-        {
-            get { return gpa; }
-            internal set { gpa = value; } 
-        }
+        public double Gpa { get; set; }
 
         public Student(string name, int studentId, int numberOfCredits, double gpa)
         {
@@ -33,6 +28,65 @@ namespace School
 
         public Student(string name, int studentId)
             : this (name, studentId, 0, 0) { }
+
+        public Student(string name)
+            : this (name, nextStudentId) 
+        {
+            nextStudentId++;
+        }
+
+        public void AddGrade(int courseCredits, double grade)
+        {
+            double currentQualityScore = Gpa * NumberOfCredits;
+            NumberOfCredits += courseCredits;
+            Gpa = (currentQualityScore + (courseCredits * grade)) / NumberOfCredits;
+        }
+
+        public string GetGradeLevel()
+        {
+            if (NumberOfCredits < 30)
+            {
+                return "freshman";
+            }
+            else if (NumberOfCredits < 60)
+            {
+                return "sophomore";
+            }
+            else if (NumberOfCredits < 90)
+            {
+                return "junior";
+            }
+            else
+            {
+                return "senior";
+            }
+        }
+
+        public override string ToString()
+        {
+            return Name + " (Credit Hours: " + NumberOfCredits + "GPA: " + Gpa + ")";
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == this)
+            {
+                return true;
+            }
+            
+            if (obj == null || obj.GetType() != GetType())
+            {
+                return false;
+            }
+
+            Student studentObj = obj as Student;
+            return StudentId == studentObj.StudentId;
+        }
+
+        public override int GetHashCode()
+        {
+            return StudentId;
+        }
     }
 
     public class Course
@@ -40,7 +94,39 @@ namespace School
         public string CourseName { get; set; }
         public string CourseCode { get; set; }
         public int CreditHours { get; set; }
-        public List<Student> StudentRoster { get; set; }
+        public List<Student> StudentRoster { get; set; } = new List<Student>();
         
+        public Course(string name, string code, int hours)
+        {
+            CourseName = name;
+            CourseCode = code;
+            CreditHours = hours;
+        }
+
+        public override string ToString()
+        {
+            return CourseName + " (Code: " + CourseCode + " Credit Hours: " + CreditHours;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == this)
+            {
+                return true;
+            }
+
+            if (obj == null || obj.GetType() != GetType())
+            {
+                return false;
+            }
+
+            Course courseObj = obj as Course;
+            return CourseCode == courseObj.CourseCode;
+        }
+
+        public override int GetHashCode()
+        {
+            return int.Parse(CourseCode);
+        }
     }
 }
